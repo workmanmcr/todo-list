fa5e8aa4697feb051c950780e734be8073e53958
workmanmcr <workmanmcr@gmail.com>
2023-07-19 10:20:39 -0700

    add delete funtion

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ToDoList.Controllers{
+    public class HomeController : Controller
+    {
+        [HttpGet("/")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
